4beb89d607eb4135f08d16d9ab311641f484e612
Art McGee <amcgee7@jaguarlandrover.com>
2014-08-05 09:16:37 -0700

    added uninstall process for the install process
    Change-Id: I2707666ffd7da89758af6cccbba4d6744ffffa07

--- InstallWgts.sh
+++ InstallWgts.sh
@@ -1,3 +1,11 @@
+#These lines remove installed wigits before installing the new ones.
+#installing wigits over existing will case problems.
+xwalkctl | egrep -e "Home Screen" | awk '{print $1}' | xargs --no-run-if-empty xwalkctl -u
+xwalkctl | egrep -e "Boilerplate" | awk '{print $1}' | xargs --no-run-if-empty xwalkctl -u
+xwalkctl | egrep -e "Browser" | awk '{print $1}' | xargs --no-run-if-empty xwalkctl -u
+xwalkctl | egrep -e "News" | awk '{print $1}' | xargs --no-run-if-empty xwalkctl -u
+#These lines install the wigits.
 xwalkctl -i /home/app/HomeScreen.wgt
 xwalkctl -i /home/app/Browser.wgt
 xwalkctl -i /home/app/Boilerplate.wgt
+xwalkctl -i /home/app/News.wgt
