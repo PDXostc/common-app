f09f0df834b104cd34ddf508cd61a3cb82b03444
Art McGee <amcgee7@jaguarlandrover.com>
2014-11-18 07:53:20 -0800

    Changes to move settings into it's own component and not exist in topbar
    Change-Id: I797883eafd8dcc69f83fa6968c4aa1884baeadf0

--- DNA_common/js/init.js
+++ DNA_common/js/init.js
@@ -1,6 +1,6 @@
 
 function includeJs(jsFilePath) {
-	console.log("includeJs"+jsFilePath);
+	console.log("includeJs "+jsFilePath);
     var js = document.createElement("script");
 
     js.type = "text/javascript";
@@ -16,12 +16,30 @@ function includeJs(jsFilePath) {
 	}
 }
 
+function includeHTML(htmlFilePath,onload,onerror) {
+	console.log("includeHTML "+htmlFilePath);
+    var html = document.createElement("link");
+
+    html.rel = "import";
+    html.href = htmlFilePath;
+    html.onload = onload;
+    html.onerror = onerror;
+
+	try {
+		document.head.appendChild(html);
+	}
+	catch (err){
+		console.log("includeHTML: "+err.message);
+	}
+}
+
 includeJs("DNA_common/components/boxCaption/boxCaption.js");
 includeJs("DNA_common/components/car/js/car.js");
 includeJs("DNA_common/js/user.js");
 includeJs("DNA_common/js/bootstrap.js");
 includeJs("DNA_common/components/bottomBar/jquery.nouislider.js");
 includeJs("DNA_common/components/bottomBar/bottomBar.js");
+includeJs("DNA_common/components/settings/js/settings.js");
 includeJs("DNA_common/components/topBar/topBar.js");
 includeJs("DNA_common/components/dateTime/dateTime.js");
 
