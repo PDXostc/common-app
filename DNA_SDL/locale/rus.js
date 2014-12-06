10cff85525e14d131a0571e0e04af89117fbf93d
Zoltan Podolyak <zpodolya@jaguarlandrover.com>
2014-12-05 17:14:03 -0800

    Added the beginnings of DNA_SDL
    Change-Id: Ia7fec0e7bae0b86b18e4cd821c12cca25258bce5

---
+++ DNA_SDL/locale/rus.js
@@ -0,0 +1,11 @@
+SDL.rus = {
+    navigation: 'Навигация',
+
+    phone: 'Звонить',
+    quickdial: 'Набор',
+    phonebook: 'Книжка',
+    history: 'История',
+    messaging: 'Сообщения',
+    settings: 'Настройки',
+    view: 'Вид'
+};
