a7fde51432cbf7e334ec0bce100914658b918e38
Art McGee <amcgee7@jaguarlandrover.com>
2014-12-10 09:57:15 -0800

    Added bluetooth settings menu
    Change-Id: I4a2dfd69f36a0e6e02385cf6afc6405d6f193482

--- DNA_common/js/init.js
+++ DNA_common/js/init.js
@@ -75,6 +75,7 @@ includeJs("DNA_common/components/rvi/js/rvi.js");
 includeJs("DNA_common/components/bottomBar/jquery.nouislider.js");
 includeJs("DNA_common/components/settings/js/settings.js");
 includeJs("DNA_common/components/wifi/js/wifi.js");
+includeJs("DNA_common/components/bluetooth/js/bluetooth.js");
 includeJs("DNA_common/components/dateTime/dateTime.js");
 
 includeJs("DNA_common/components/audioPlayer/most.js");
