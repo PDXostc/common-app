5a754cfe8ba057f3a8510fd471231b47fbe30d5b
Art McGee <amcgee7@jaguarlandrover.com>
2015-03-20 16:07:46 -0700

    fresh start
    Change-Id: I0f36782b60e2479b78e6e609f69881e303805976

--- AudioControl/README.txt
+++
@@ -1,3 +0,0 @@
-To build this requires the css folder, which is common to
-all JLR Tizen widget apps and is kept in a common place
-somewhere in our source archives.
--- DNA_Browser/js/search.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	var cx = '001529786138519409299:lnssp3uguay';
-	var gcse = document.createElement('script');
-	gcse.type = 'text/javascript';
-	gcse.async = true;
-	gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//www.google.com/cse/cse.js?cx=' + cx;
-	var s = document.getElementsByTagName('script')[0];
-	s.parentNode.insertBefore(gcse, s);
-})();
