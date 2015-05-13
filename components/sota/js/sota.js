70542bf98bab8b93c47f7d3fbfdb624a34b51ee3
Barbara Harmon <bharmon@jaguarlandrover.com>
2015-05-13 11:55:31 -0700

    Added new sota component with sota.html that has the demo popup,  css folder and sota.css, and js folder with sota.js. Signed-off-by: Barbara Harmon <bharmon@jaguarlandrover.com>

---
+++ components/sota/js/sota.js
@@ -0,0 +1,9 @@
+$("#close-sota").click(function() {
+  if ($("#modal").hasClass("open")) {
+    $("#modal").addClass("hidden");
+  }  
+  else {
+    $("#modal").addClass("open")) {
+    $("#modal").removeClass("hidden");
+  }
+}
